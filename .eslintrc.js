62ada44374ffdfe4c80a3db0aecf436ef7dcabdb
HossamElDeenKamal2003 <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-08 23:04:22 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    'prettier/prettier':0,
+  },
+};
